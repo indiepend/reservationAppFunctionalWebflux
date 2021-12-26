b8de0b2c68c60eca877a064349a7c2efb1002638
Robert <robert.p.dworak@gmail.com>
2021-12-26 01:05:33 +0100

    add files

---
+++ src/main/java/me/Dworak/reservationAppFunctionalWebflux/ReservationAppFunctionalWebfluxApplication.java
@@ -0,0 +1,13 @@
+package me.Dworak.reservationAppFunctionalWebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationAppFunctionalWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationAppFunctionalWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/Dworak/reservationAppFunctionalWebflux/ReservationAppFunctionalWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package me.Dworak.reservationAppFunctionalWebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationAppFunctionalWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
